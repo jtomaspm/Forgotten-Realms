83e7745a0d7487cf3dd2c39dd8c69dcea294228b
João Machado <joaotomachado@proton.me>
2025-02-16 08:55:12 +0000

    added migrations

--- database/migrations/SimplifiedCrafter/001-create/002-accounts.sql
+++ database/migrations/SimplifiedCrafter/001-create/002-accounts.sql
@@ -2,6 +2,7 @@ CREATE TABLE `Roles` (
     `Name` VARCHAR(50) PRIMARY KEY
 );
 
+
 CREATE TABLE `Accounts` (
     `Id`            CHAR(36)        PRIMARY KEY DEFAULT (UUID()),
     `ExternalId`    VARCHAR(255)    UNIQUE,
@@ -9,5 +10,18 @@ CREATE TABLE `Accounts` (
     `Email`         VARCHAR(255)    UNIQUE NOT NULL,
     `Role`          VARCHAR(50)     NOT NULL,
     `CreatedAt`     TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    `UpdatedAt`     TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`Role`) REFERENCES `Roles`(`Name`) ON DELETE CASCADE
+);
+
+
+CREATE TABLE `AccountProperties` (
+    `AccountId`                 CHAR(36)        PRIMARY KEY,
+    `VerificationToken`         CHAR(36)        UNIQUE,
+    `TokenExpiresAt`            TIMESTAMP,
+    `EmailVerified`             BOOLEAN         DEFAULT FALSE,
+    `SendEmailNotifications`    BOOLEAN         DEFAULT FALSE,
+    `CreatedAt`                 TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    `UpdatedAt`                 TIMESTAMP       DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (`AccountId`) REFERENCES `Accounts`(`Id`) ON DELETE CASCADE
 );
