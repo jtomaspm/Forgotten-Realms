398b0d15569095ea0b2359d6e3661c4db96665ba
João Machado <joaotomachado@proton.me>
2025-02-18 15:49:54 +0000

    added global exception handler

---
+++ csharp/ApiUtils/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace ApiUtils.Middleware;
+
+public class GlobalExceptionHandler(RequestDelegate next, ILogger<GlobalExceptionHandler> logger)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Unhandled exception caught by GlobalExceptionHandler.");
+            await HandleExceptionAsync(context);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int) HttpStatusCode.InternalServerError;
+        await context.Response.WriteAsync(JsonSerializer.Serialize(new { Message = "Internal server error." }));
+    }
+
+}
--- csharp/Database/ApplicationDatabase/Services/AccountService.cs
+++ csharp/Database/ApplicationDatabase/Services/AccountService.cs
@@ -4,7 +4,7 @@ namespace Database.ApplicationDatabase.Services;
 
 public class AccountService
 {
-    public static Account CreateAccout(Database database, string source, string externalId, string name, string email, Role role)
+    public static async Task<Account> CreateAccount(Database database, string source, string externalId, string name, string email, Role role)
     {
         var account = new Account
         {
@@ -15,8 +15,31 @@ public class AccountService
             Role = role
         };
 
-        var connetion = database.GetConnection();
+        var connection = await database.GetConnectionAsync();
+        using (var transaction = connection.BeginTransaction() ?? throw new Exception("Error creating database transaction"))
+        {
+            try 
+            {
+                var cmd = connection.CreateCommand();
+                cmd.CommandText = "SELECT `Id`, `Name` FROM `Accounts` WHERE Email=@email";
+                cmd.Parameters.AddWithValue("email", "joaotomachado@proton.me");
 
+                using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                {
+                        var accountId = reader.GetGuid(0);
+                        var accountName = reader.GetString(1);
+                        Console.WriteLine($"$$$$$$$$$$$$[Account]$$$$$$$$$\nId:{accountId}\nName:{accountName}");
+                }
+
+                await transaction.CommitAsync();
+            } 
+            catch (Exception e) 
+            {
+                await transaction.RollbackAsync();
+                throw new Exception("Create Account Transaction failed", e);
+            }
+        }
 
         return account;
     }
