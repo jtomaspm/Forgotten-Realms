97a6afbd77510ef1375741801ae0f040df447416
João Machado <joaotomachado@proton.me>
2025-02-19 21:03:54 +0000

    fix lambdas

--- csharp/Database/CommandBuilder/DatabaseCommandBuilder.cs
+++ csharp/Database/CommandBuilder/DatabaseCommandBuilder.cs
@@ -9,7 +9,7 @@ public static class DatabaseCommandBuilder
     public static MySqlCommand Select(this MySqlCommand cmd, IEnumerable<string> fields, string table, IEnumerable<string>? conditions, Dictionary<string, object>? parameters)
     {
         var conditionString = conditions is null ? "" : $"WHERE {ToConditionListString(conditions)}";
-        var query = @$"SELECT {ToFieldListString(fields)} FROM {GetSafePropName(table)} {conditionString};";
+        var query = @$"SELECT {ToFieldnameListString(fields)} FROM {GetSafePropName(table)} {conditionString};";
 
         cmd.CommandText = query;
         if (parameters is not null)
@@ -21,7 +21,7 @@ public static class DatabaseCommandBuilder
     }
     public static MySqlCommand Insert(this MySqlCommand cmd, IEnumerable<string> fields, string table, IEnumerable<IEnumerable<string>> values, Dictionary<string, object>? parameters)
     {
-        var query = @$"INSERT INTO {GetSafePropName(table)} {GetSafeConditionName(ToFieldListString(fields))} VALUES {table} {GetInsertValuesString(values)};";
+        var query = @$"INSERT INTO {GetSafePropName(table)} {GetSafeConditionName(ToFieldnameListString(fields))} VALUES {table} {GetInsertValuesString(values)};";
 
         cmd.CommandText = query;
         if (parameters is not null)
@@ -35,10 +35,12 @@ public static class DatabaseCommandBuilder
         => $"`{name}`";
     private static string GetSafeConditionName (string name) 
         => $"({name})";
-    private static string ToFieldListString(IEnumerable<string> fields) 
-        => new StringBuilder().AppendJoin(", ", fields.Select(x=>GetSafePropName(x))).ToString();
+    private static string ToValueListString(IEnumerable<string> values) 
+        => GetSafeConditionName(new StringBuilder().AppendJoin(", ", values).ToString());
+    private static string ToFieldnameListString(IEnumerable<string> fields) 
+        => new StringBuilder().AppendJoin(", ", fields.Select(GetSafePropName)).ToString();
     private static string ToConditionListString(IEnumerable<string> conditions) 
-        => new StringBuilder().AppendJoin(" AND ", conditions.Select(x=>GetSafeConditionName(x))).ToString();
+        => new StringBuilder().AppendJoin(" AND ", conditions.Select(GetSafeConditionName)).ToString();
     private static string GetInsertValuesString(IEnumerable<IEnumerable<string>> values)
-        => ToFieldListString(values.Select(valueList => GetSafeConditionName(ToFieldListString(valueList))));
+        => ToFieldnameListString(values.Select(ToValueListString));
 }
